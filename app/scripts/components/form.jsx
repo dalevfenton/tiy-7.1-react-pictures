86f73d5817e078f5685b52d0f93ae115c7e03696
Dale Fenton <fentondale@gmail.com>
2016-03-15 00:26:16 -0400

    added very rough form validation on new post form

--- app/scripts/components/form.jsx
+++ app/scripts/components/form.jsx
@@ -4,23 +4,39 @@ var ImageForm = React.createClass({
   getInitialState: function() {
     return {
       formToggle: false,
+      urlError: false,
+      captionError: false,
       url_src: '',
       caption: ''
     };
   },
   handleURLInput: function(e) {
     this.setState({url_src: e.target.value});
+    var url_src = this.state.url_src;
+    console.log(!url_src);
+    console.log(url_src.indexOf('http') !== 1);
+    console.log(url_src.length > 5);
+    if((!url_src )|| (url_src.indexOf('http') !== 0 && url_src.length > 5) ){
+      this.setState({urlError: true});
+      return;
+    }else{
+      this.setState({urlError: false});
+    }
   },
   handleCaptionInput: function(e) {
     this.setState({caption: e.target.value});
+    var caption = this.state.caption;
+    if (!caption){
+      this.setState({captionError: true});
+      return;
+    }else{
+      this.setState({captionError: false});
+    }
   },
   handleSubmit: function(e) {
     e.preventDefault();
     var url_src = this.state.url_src;
     var caption = this.state.caption;
-    if (!url_src || !caption) {
-      return;
-    }
     this.props.collection.create({url_src: url_src, caption: caption}, {wait: true});
     this.setState({url_src: '', caption: '', formToggle: false});
   },
@@ -30,11 +46,21 @@ var ImageForm = React.createClass({
     });
   },
   render: function(){
-    var formClass;
+    var formClass, urlClass, captionClass;
     if(this.state.formToggle){
       formClass = 'new-image-form-holder';
     }else{
       formClass = 'new-image-form-holder hidden';
+    }
+    if(this.state.urlError){
+      urlClass = 'form-group has-error';
+    }else{
+      urlClass = 'form-group';
+    }
+    if(this.state.captionError){
+      captionClass = 'form-group has-error';
+    }else{
+      captionClass = 'form-group';
     }
     return (
       <div className="row">
@@ -48,22 +74,26 @@ var ImageForm = React.createClass({
         <div className={formClass}>
           <div className="container">
             <form id="new-image-form" className="image-form" onSubmit={this.handleSubmit}>
-              <input
-                type="url"
-                className="form-control"
-                name="url_src"
-                placeholder="Image URL"
-                value={this.state.url_src}
-                onChange={this.handleURLInput}
-              />
-              <textarea
-                type="text"
-                className="form-control"
-                name="caption"
-                placeholder="Image Caption"
-                value={this.state.caption}
-                onChange={this.handleCaptionInput}
-              ></textarea>
+              <div className={urlClass}>
+                <input
+                  type="url"
+                  className="form-control"
+                  name="url_src"
+                  placeholder="Image URL"
+                  value={this.state.url_src}
+                  onChange={this.handleURLInput}
+                />
+              </div>
+              <div className={captionClass}>
+                <textarea
+                  type="text"
+                  className={captionClass}
+                  name="caption"
+                  placeholder="Image Caption"
+                  value={this.state.caption}
+                  onChange={this.handleCaptionInput}
+                ></textarea>
+              </div>
               <button
                 className="submit btn btn-success"
                 type="submit"
--- app/scripts/index.js
+++ app/scripts/index.js
@@ -14,15 +14,16 @@ var Page = require('./components/page.jsx');
 // var cached = [{"_id":"56e776dfa3ad3d0300001820","url_src":"http://www.bt-images.net/wp-content/uploads/2014/02/summer-sunset.jpg","caption":"A mysterious marsh by Trey Ratcliff via Flickr","created":"2016-03-15T02:43:43.132Z"},{"_id":"56e7768ea3ad3d030000181f","url_src":"http://www.bt-images.net/wp-content/uploads/2014/02/Badabeh-surt.jpg","caption":"Badab Soort Natural Spring, Mazandaran, Iran by Ninara via Flickr","created":"2016-03-15T02:42:22.714Z"},{"_id":"56e7759fa3ad3d030000181c","url_src":"http://www.bt-images.net/wp-content/uploads/2014/02/Geyser.jpg","caption":"Fly Geyser via Wikimedia Commons (Nevada, USA)","created":"2016-03-15T02:38:23.563Z"},{"_id":"56e77221a3ad3d0300001819","url_src":"https://googleplus-covers.com/covers/beautiful_morning.jpg","caption":"Google Cover Plus - Really Awesome Beautiful Morning","created":"2016-03-15T02:35:10.335Z"},{"_id":"56e77221a3ad3d0300001818","url_src":"https://lh5.ggpht.com/EsIOw_M7lK9HVWmsZSura5oZL7iucO0ctNuj3xvCecMye0y3FUfVEQJXLpeFXVWUPg=h900","caption":"Beautiful Landscape Puzzle - Really Awesome Leaves","created":"2016-03-15T02:35:10.336Z"},{"_id":"56e77221a3ad3d0300001817","url_src":"https://lh3.ggpht.com/iqBXuNgqZoyPMwXSY7ENa4zNmWH_DUIZ55eIbdPIiyhMXRkPhBTJsEJctPX8FA4wkg=h900","caption":"Beautiful Landscape Puzzle - Really Awesome Waterfall","created":"2016-03-15T02:35:10.336Z"},{"_id":"56e771f1a3ad3d0300001816","url_src":"https://lh5.ggpht.com/EsIOw_M7lK9HVWmsZSura5oZL7iucO0ctNuj3xvCecMye0y3FUfVEQJXLpeFXVWUPg=h900","caption":"Beautiful Landscape Puzzle - Really Awesome Leaves","created":"2016-03-15T02:34:36.463Z"},{"_id":"56e771f1a3ad3d0300001815","url_src":"https://googleplus-covers.com/covers/beautiful_morning.jpg","caption":"Google Cover Plus - Really Awesome Beautiful Morning","created":"2016-03-15T02:35:10.337Z"},{"_id":"56e771f1a3ad3d0300001814","url_src":"https://lh3.ggpht.com/iqBXuNgqZoyPMwXSY7ENa4zNmWH_DUIZ55eIbdPIiyhMXRkPhBTJsEJctPX8FA4wkg=h900","caption":"Beautiful Landscape Puzzle - Really Awesome Waterfall","created":"2016-03-15T02:35:10.337Z"},{"_id":"56e770ada3ad3d0300001812","url_src":"https://s-media-cache-ak0.pinimg.com/736x/ff/55/50/ff55508083fcdc3e3c30b9990fcc04e7.jpg","caption":"beautiful landscapes - Google Search","created":"2016-03-15T02:34:36.463Z"}];
 var images = new models.ImageCollection( );
 
+//used to upload images from dummy or from cache after deleting
 // images.each(function(model){
 //   model.save();
 // })
-console.log(images);
+// console.log(images);
+
+
 images.fetch().done(function(){
   ReactDOM.render(
     <Page collection={images} />,
     document.getElementById( 'app' )
   );
 });
-
-// console.log( images );
